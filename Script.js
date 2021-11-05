88e9d315e41c826ff0dd601416754d11c3e5d6f7
MdSifatulla <89011586+MdSifatulla@users.noreply.github.com>
2021-11-05 10:50:53 +0600

    Updating

--- Script.js
+++ Script.js
@@ -1,7 +1,14 @@
+// Login button event handler
 const signIn = document.getElementById('signin');
 signIn.addEventListener('click', function () {
     const loginArea = document.getElementById('login-area')
      loginArea.style.display = 'none'
      const transection = document.getElementById('Transection-area');
      transection.style.display = 'block';
+})
+
+// Deposit button event handler
+const depositAction = document.getElementById('depositBtn');
+depositAction.addEventListener('click', function () {
+    console.log('clicked is working');
 })
