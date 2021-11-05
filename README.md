fb97dcb4e5b6d7c1139dd26f134460bf6d2a9feb
MdSifatulla <89011586+MdSifatulla@users.noreply.github.com>
2021-11-05 22:53:34 +0600

    Updating

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Banking_System
+echo "# Shopping_Cart" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/MdSifatulla/Shopping_Cart.git
+git push -u origin main
+
+git remote add origin https://github.com/MdSifatulla/Shopping_Cart.git
+git branch -M main
+git push -u origin main
--- Script.js
+++ Script.js
@@ -12,18 +12,39 @@ const depositBtn = document.getElementById('depositBtn');
 depositBtn.addEventListener('click', function () {
     const depositAmount = document.getElementById('inputDeposit').value;
     const depositNumber = parseFloat(depositAmount);
-    console.log('click is working')
-
-    const currentMoney = document.getElementById('depositAmount').innerText;
-    const currentNumber = parseFloat(currentMoney);
-    const showAmount = depositNumber + currentNumber;
+    // const currentMoney = document.getElementById('depositAmount').innerText;
+    // const currentNumber = parseFloat(currentMoney);
+    // const showAmount = depositNumber + currentNumber;
     
-    document.getElementById('depositAmount').innerText = showAmount;
+    // document.getElementById('depositAmount').innerText = showAmount;
+    // document.getElementById('inputDeposit').value = "";
+
+    // const currentBalance = document.getElementById('currentBalance').innerText;
+    // const currentBalanceNumber = parseFloat(currentBalance);
+    // const totalBalance = currentBalanceNumber + showAmount
+    // document.getElementById('currentBalance').innerText = totalBalance;
+    span_Text('depositAmount', depositNumber);
+    span_Text('currentBalance', depositNumber )
     document.getElementById('inputDeposit').value = "";
-
-    const currentBalance = document.getElementById('currentBalance').innerText;
-    const currentBalanceNumber = parseFloat(currentBalance);
-    const totalBalance = currentBalanceNumber + showAmount
-    document.getElementById('currentBalance').innerText = totalBalance;
-    
 })
+// Withdraw Event Handler
+const withdrawBtn = document.getElementById('withdrawBtn');
+withdrawBtn.addEventListener('click', function(){
+    const withdrawValue1 = getInputNumber('withdrawInput')
+    
+    span_Text('displayWithdrawAmount', withdrawValue1);
+    span_Text('currentBalance', -1 * withdrawValue1);
+})
+
+function getInputNumber() {
+    const withdrawInput = document.getElementById('withdrawInput').value;
+    const withdrawValue = parseFloat(withdrawInput);
+    return withdrawValue;
+}
+function span_Text(id, depositNumber) {
+    const currentMoney = document.getElementById(id).innerText;
+    const currentNumber = parseFloat(currentMoney);
+    const totalBalance = depositNumber + currentNumber;
+    
+    document.getElementById(id).innerText = totalBalance;
+}
